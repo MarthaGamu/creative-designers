04efff77fe9d3e2dbc197855158f16b8f9ab867e
Martha Mandizvidza <marthagmandizvidza60@gmail.com>
2019-03-19 17:52:03 +0000

    intial commit

---
+++ mcgweb/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>MC Website Designers</title>
+</head>
+<body>
+<script type ="text/javascript" scr="jquery.1.js"></script>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap/js/bootstrap.min.js">
+<link rel="stylesheet"  href ="style/styles.css">
+
+<div class= "container">
+<header>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">MCGWEB</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="index.php">HOME <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="about.php">ABOUT US </a>
+      <a class="nav-item nav-link" href="blog.php">BLOG</a>
+      <a class="nav-item nav-link" href="services.php">SERVICES </a>
+      <a class="nav-item nav-link" href="portfolio.php">PORTFOLIO</a>
+      <a class="nav-item nav-link" href="contact.php">CONTACT </a>
+      
+    </div>
+  </div>
+</nav>
+</header>
+</div>
---
+++ mcgweb/index.php
@@ -0,0 +1,17 @@
+<?php 
+include'header.php';
+?>
+
+
+<div class="container" id="myvideo">
+    <div id ="row">
+<video preload="true" autoplay="autoplay" loop="loop" volume="0">
+<source src="video/homevideo.mp4" type ="video/mp4">
+</video>
+</div>
+</div>
+<?php
+include'footer.php';
+
+
+?>
